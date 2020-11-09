d7aebf85546f0112f0d5c2503d241870629fa2fe
Kieran Daly <kierandaly@Kierans-MacBook-Air.local>
2020-11-09 18:50:48 +0000

    removed secret key

--- weme/settings.py
+++ weme/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'hz#&*+efe^7)c1qxc-57u)2t$bvl^)h=cnufy8w-rv$&qlf3y2'
+
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
