97953489650479b0e3f57700c3c288a0efd01957
Kieran Daly <kierandaly@Kierans-MacBook-Air.local>
2020-11-09 11:35:03 +0000

    added new photos

--- weme/urls.py
+++ weme/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('feed.urls')),
+    # path('', include('feed.urls')),
+    path('feed/', include('feed.urls')),
     path('users/', user_views.users_list, name='users_list'),
     path('users/<slug>/', user_views.profile_view, name='profile_view'),
     path('friends/', user_views.friend_list, name='friend_list'),
@@ -35,7 +36,8 @@ urlpatterns = [
     path('edit-profile/', user_views.edit_profile, name='edit_profile'),
     path('my-profile/', user_views.my_profile, name='my_profile'),
     path('search_users/', user_views.search_users, name='search_users'),
-    path('register/', user_views.register, name='register'),
+    # path('register/', user_views.register, name='register'),
+    path('', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
